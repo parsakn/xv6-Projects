d89b2df490ed6e930ee464d9559afdedee7cee13
root <root@DESKTOP-OQS77JQ.>
2025-03-08 23:00:46 +0330

    Initial commit: Set up xv6 project - first task done.

--- init.c
+++ init.c
@@ -21,6 +21,10 @@ main(void)
 
   for(;;){
     printf(1, "init: starting sh\n");
+    printf(1, "Group #9\n");
+    printf(1, "ParsaKhoshnazari\n");
+    printf(1, "MahdiDoustMohammadi\n");
+    printf(1, "YasaminAkhavein\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
